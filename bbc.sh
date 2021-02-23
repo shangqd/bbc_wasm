01c2cb442b7afeb54be55818ddb5e8b9032a2bc3
shangqd <shang_qd@qq.com>
2021-02-23 10:50:17 +0800

    v1

--- bbc.sh
+++ bbc.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-export BOOST_ROOT=/home/shang/eosio/1.8/src/boost_1_70_0
+export BOOST_ROOT=~/eosio/1.8/src/boost_1_70_0
 
 rm -rf build
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Debug -DCORE_SYMBOL_NAME=SYS -DCMAKE_INSTALL_PREFIX=/home/shang/eosio/1.8 -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DCMAKE_PREFIX_PATH=/home/shang/eosio/1.8  ..
-#cmake -DCMAKE_BUILD_TYPE=Release -DCORE_SYMBOL_NAME=SYS -DCMAKE_INSTALL_PREFIX=/home/shang/eosio/1.8 -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc -DCMAKE_PREFIX_PATH=/home/shang/eosio/1.8  ..
+#cmake -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DCMAKE_PREFIX_PATH=~/eosio/1.8  ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DCMAKE_PREFIX_PATH=~/eosio/1.8  ..
+#cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc -DCMAKE_PREFIX_PATH=~/eosio/1.8  ..
--- libraries/chain/wasm_interface.cpp
+++ libraries/chain/wasm_interface.cpp
@@ -16,12 +16,9 @@
 #include <fc/exception/exception.hpp>
 #include <fc/crypto/sha256.hpp>
 #include <fc/crypto/sha1.hpp>
-#include <fc/io/raw.hpp>
 
 #include <softfloat.hpp>
 #include <compiler_builtins.hpp>
-#include <boost/asio.hpp>
-#include <boost/bind.hpp>
 #include <fstream>
 #include <string.h>
 
